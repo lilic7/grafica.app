bbe460402b4f4930497776189247df7099f8da4b
lilic7 <zubcolilian@mail.ru>
2016-06-30 20:45:12 +0300

    refactoring TimerService

--- app/shared/timer/timerService.coffee
+++ app/shared/timer/timerService.coffee
@@ -1,95 +1,198 @@
 (->
-  TimerService = ($interval, ErrorService, SettingsService)->
-    startTime = 10 #seconds
-    totalSeconds = 10
-    totalMinutes = "00"
-    playMinutes = "01"
-    timerIsRunning = false
-    time = "00:10"
-    timerInterval = null
+  class TimerService
+    constructor: (@$interval, @ErrorService, @SettingsService) ->
 
-    @add              = add
-    @addSeconds       = addSeconds
-    @getPlayMinutes   = getPlayMinutes
-    @getTime          = getTime
-    @getTotalMinutes  = getTotalMinutes
-    @isOn             = isOn
-    @modify           = modify
-    @start            = start
-    @stop             = stop
-    @sub              = sub
-
-
-    isOn = =>
-      timerIsRunning
-
-    getTotalMinutes = =>
-      totalMinutes
-
-    getPlayMinutes = =>
-      playMinutes
-
-    getTime = =>
-      time
-
-    start = =>
-      timerInterval = $interval timer, 1000 if not timerIsRunning
+    add: add
+    addSeconds: addSeconds
+    getPlayMinutes: getPlayMinutes
+    getTime: getTime
+    getTotalMinutes: getTotalMinutes
+    isOn: isOn
+    modify: modify
+    sub: sub
+    start: ($interval)->
+      timerInterval = interval timer, 1000 if not timerIsRunning
       return
-
-    stop = =>
-      $interval.cancel timerInterval
+    stop: (interval)->
+      interval.cancel timerInterval
       timerIsRunning = false
       return
 
-    modify = (minutes)=>
-      seconds = minutes * 60
-      totalSeconds = seconds + startTime
+  #############################################
+
+  startTime = 10
+  totalSeconds = 10
+  totalMinutes = "00"
+  playMinutes = "01"
+  timerIsRunning = false
+  time = "00:10"
+  timerInterval = null
+
+  isOn = ->
+    timerIsRunning
+
+  getTotalMinutes = ->
+    totalMinutes
+
+  getPlayMinutes = ->
+    playMinutes
+
+  getTime = ->
+    time
+
+  start = (interval)->
+    timerInterval = interval timer, 1000 if not timerIsRunning
+    return
+
+  stop = (interval)->
+    interval.cancel timerInterval
+    timerIsRunning = false
+    return
+
+  modify = (minutes)->
+    seconds = minutes * 60
+    totalSeconds = seconds + startTime
+    calculateTime()
+    return
+
+  add = (minutes)->
+    if(totalSeconds > 600 and timerIsRunning)
+      ErrorService.setMessage "MATCH_TOO_LONG"
+    totalSeconds += minutes * 60
+    calculateTime()
+    return
+
+  sub = (minutes)->
+    seconds = minutes * 60
+    if totalSeconds > seconds
+      totalSeconds -= seconds
       calculateTime()
-      return
+    else
+      ErrorService.setMessage "NEGATIVE_TIME"
+    return
 
-    add = (minutes)=>
-      if(totalSeconds > 600 and timerIsRunning)
-        ErrorService.setMessage "MATCH_TOO_LONG"
-      totalSeconds += minutes * 60
-      calculateTime()
-      return
+  addSeconds = (seconds)->
+    totalSeconds += seconds
+    calculateTime()
+    return
 
-    sub = (minutes)=>
-      seconds = minutes * 60
-      if totalSeconds > seconds
-        totalSeconds -= seconds
-        calculateTime()
-      else
-        ErrorService.setMessage "NEGATIVE_TIME"
-      return
+  timer = ->
+    ++totalSeconds
+    calculateTime()
+    timerIsRunning = true
+    return
 
-    addSeconds = (seconds)=>
-      totalSeconds += seconds
-      calculateTime()
-      return
+  toMinutes = (seconds)->
+    minutes = Math.floor seconds / 60
+    if minutes < 10 then '0' + minutes else minutes
 
-    timer = ->
-      ++totalSeconds
-      calculateTime()
-      timerIsRunning = true
-      return
-
-    toMinutes = (seconds)->
-      minutes = Math.floor seconds / 60
-      if minutes < 10 then '0' + minutes else minutes
-
-    calculateTime = ()->
-      seconds = totalSeconds % 60
-      totalMinutes = toMinutes totalSeconds
-      playMinutes = toMinutes totalSeconds + 60
-      time = totalMinutes + ":" + (if seconds < 10 then '0' + seconds else seconds)
-      return
-
+  calculateTime = ()->
+    seconds = totalSeconds % 60
+    totalMinutes = toMinutes totalSeconds
+    playMinutes = toMinutes totalSeconds + 60
+    time = totalMinutes + ":" + (if seconds < 10 then '0' + seconds else seconds)
     return
 
   TimerService.$inject = ['$interval', 'ErrorService', 'SettingsService']
-
   angular
     .module "timer.service", []
-    .service "TimerService", TimerService
+    .factory "TimerService", ()-> new TimerService()
 )()
+
+
+#(->
+#  startTime = 10 #seconds
+#  totalSeconds = 10
+#  totalMinutes = "00"
+#  playMinutes = "01"
+#  timerIsRunning = false
+#  time = "00:10"
+#  timerInterval = null
+#
+#  TimerService = ($interval, ErrorService, SettingsService)->
+#
+#    @add              = add
+#    @addSeconds       = addSeconds
+#    @getPlayMinutes   = getPlayMinutes
+#    @getTime          = getTime
+#    @getTotalMinutes  = getTotalMinutes
+#    @isOn             = isOn
+#    @modify           = modify
+#    @sub              = sub
+#    @start            = startTimer $interval
+#    @stop             = stopTimer $interval
+#
+#
+#    return
+#
+#  isOn = ->
+#    timerIsRunning
+#
+#  getTotalMinutes = ->
+#    totalMinutes
+#
+#  getPlayMinutes = ->
+#    playMinutes
+#
+#  getTime = ->
+#    time
+#
+#  startTimer = (interval)->
+#    timerInterval = interval timer, 1000 if not timerIsRunning
+#    return
+#
+#  stopTimer = (interval)->
+#    interval.cancel timerInterval
+#    timerIsRunning = false
+#    return
+#
+#  modify = (minutes)->
+#    seconds = minutes * 60
+#    totalSeconds = seconds + startTime
+#    calculateTime()
+#    return
+#
+#  add = (minutes)->
+#    if(totalSeconds > 600 and timerIsRunning)
+#      ErrorService.setMessage "MATCH_TOO_LONG"
+#    totalSeconds += minutes * 60
+#    calculateTime()
+#    return
+#
+#  sub = (minutes)->
+#    seconds = minutes * 60
+#    if totalSeconds > seconds
+#      totalSeconds -= seconds
+#      calculateTime()
+#    else
+#      ErrorService.setMessage "NEGATIVE_TIME"
+#    return
+#
+#  addSeconds = (seconds)->
+#    totalSeconds += seconds
+#    calculateTime()
+#    return
+#
+#  timer = ->
+#    ++totalSeconds
+#    calculateTime()
+#    timerIsRunning = true
+#    return
+#
+#  toMinutes = (seconds)->
+#    minutes = Math.floor seconds / 60
+#    if minutes < 10 then '0' + minutes else minutes
+#
+#  calculateTime = ()->
+#    seconds = totalSeconds % 60
+#    totalMinutes = toMinutes totalSeconds
+#    playMinutes = toMinutes totalSeconds + 60
+#    time = totalMinutes + ":" + (if seconds < 10 then '0' + seconds else seconds)
+#    return
+#
+#  TimerService.$inject = ['$interval', 'ErrorService', 'SettingsService']
+#
+#  angular
+#    .module "timer.service", []
+#    .service "TimerService", TimerService
+#)()
--- public/js/app.js
+++ public/js/app.js
@@ -368,118 +368,126 @@ angular.module("timer.directive", ['timer.controller']).directive("timer", funct
 });
 
 (function() {
-  var TimerService;
-  TimerService = function($interval, ErrorService, SettingsService) {
-    var add, addSeconds, calculateTime, getPlayMinutes, getTime, getTotalMinutes, isOn, modify, playMinutes, start, startTime, stop, sub, time, timer, timerInterval, timerIsRunning, toMinutes, totalMinutes, totalSeconds;
-    startTime = 10;
-    totalSeconds = 10;
-    totalMinutes = "00";
-    playMinutes = "01";
-    timerIsRunning = false;
-    time = "00:10";
-    timerInterval = null;
-    this.add = add;
-    this.addSeconds = addSeconds;
-    this.getPlayMinutes = getPlayMinutes;
-    this.getTime = getTime;
-    this.getTotalMinutes = getTotalMinutes;
-    this.isOn = isOn;
-    this.modify = modify;
-    this.start = start;
-    this.stop = stop;
-    this.sub = sub;
-    isOn = (function(_this) {
-      return function() {
-        return timerIsRunning;
-      };
-    })(this);
-    getTotalMinutes = (function(_this) {
-      return function() {
-        return totalMinutes;
-      };
-    })(this);
-    getPlayMinutes = (function(_this) {
-      return function() {
-        return playMinutes;
-      };
-    })(this);
-    getTime = (function(_this) {
-      return function() {
-        return time;
-      };
-    })(this);
-    start = (function(_this) {
-      return function() {
-        if (!timerIsRunning) {
-          timerInterval = $interval(timer, 1000);
-        }
-      };
-    })(this);
-    stop = (function(_this) {
-      return function() {
-        $interval.cancel(timerInterval);
-        timerIsRunning = false;
-      };
-    })(this);
-    modify = (function(_this) {
-      return function(minutes) {
-        var seconds;
-        seconds = minutes * 60;
-        totalSeconds = seconds + startTime;
-        calculateTime();
-      };
-    })(this);
-    add = (function(_this) {
-      return function(minutes) {
-        if (totalSeconds > 600 && timerIsRunning) {
-          ErrorService.setMessage("MATCH_TOO_LONG");
-        }
-        totalSeconds += minutes * 60;
-        calculateTime();
-      };
-    })(this);
-    sub = (function(_this) {
-      return function(minutes) {
-        var seconds;
-        seconds = minutes * 60;
-        if (totalSeconds > seconds) {
-          totalSeconds -= seconds;
-          calculateTime();
-        } else {
-          ErrorService.setMessage("NEGATIVE_TIME");
-        }
-      };
-    })(this);
-    addSeconds = (function(_this) {
-      return function(seconds) {
-        totalSeconds += seconds;
-        calculateTime();
-      };
-    })(this);
-    timer = function() {
-      ++totalSeconds;
-      calculateTime();
-      timerIsRunning = true;
-    };
-    toMinutes = function(seconds) {
-      var minutes;
-      minutes = Math.floor(seconds / 60);
-      if (minutes < 10) {
-        return '0' + minutes;
-      } else {
-        return minutes;
+  var TimerService, add, addSeconds, calculateTime, getPlayMinutes, getTime, getTotalMinutes, isOn, modify, playMinutes, start, startTime, stop, sub, time, timer, timerInterval, timerIsRunning, toMinutes, totalMinutes, totalSeconds;
+  TimerService = (function() {
+    function TimerService($interval1, ErrorService1, SettingsService) {
+      this.$interval = $interval1;
+      this.ErrorService = ErrorService1;
+      this.SettingsService = SettingsService;
+    }
+
+    TimerService.prototype.add = add;
+
+    TimerService.prototype.addSeconds = addSeconds;
+
+    TimerService.prototype.getPlayMinutes = getPlayMinutes;
+
+    TimerService.prototype.getTime = getTime;
+
+    TimerService.prototype.getTotalMinutes = getTotalMinutes;
+
+    TimerService.prototype.isOn = isOn;
+
+    TimerService.prototype.modify = modify;
+
+    TimerService.prototype.sub = sub;
+
+    TimerService.prototype.start = function($interval) {
+      var timerInterval;
+      if (!timerIsRunning) {
+        timerInterval = interval(timer, 1000);
       }
     };
-    calculateTime = function() {
-      var seconds;
-      seconds = totalSeconds % 60;
-      totalMinutes = toMinutes(totalSeconds);
-      playMinutes = toMinutes(totalSeconds + 60);
-      time = totalMinutes + ":" + (seconds < 10 ? '0' + seconds : seconds);
+
+    TimerService.prototype.stop = function(interval) {
+      var timerIsRunning;
+      interval.cancel(timerInterval);
+      timerIsRunning = false;
     };
+
+    return TimerService;
+
+  })();
+  startTime = 10;
+  totalSeconds = 10;
+  totalMinutes = "00";
+  playMinutes = "01";
+  timerIsRunning = false;
+  time = "00:10";
+  timerInterval = null;
+  isOn = function() {
+    return timerIsRunning;
+  };
+  getTotalMinutes = function() {
+    return totalMinutes;
+  };
+  getPlayMinutes = function() {
+    return playMinutes;
+  };
+  getTime = function() {
+    return time;
+  };
+  start = function(interval) {
+    if (!timerIsRunning) {
+      timerInterval = interval(timer, 1000);
+    }
+  };
+  stop = function(interval) {
+    interval.cancel(timerInterval);
+    timerIsRunning = false;
+  };
+  modify = function(minutes) {
+    var seconds;
+    seconds = minutes * 60;
+    totalSeconds = seconds + startTime;
+    calculateTime();
+  };
+  add = function(minutes) {
+    if (totalSeconds > 600 && timerIsRunning) {
+      ErrorService.setMessage("MATCH_TOO_LONG");
+    }
+    totalSeconds += minutes * 60;
+    calculateTime();
+  };
+  sub = function(minutes) {
+    var seconds;
+    seconds = minutes * 60;
+    if (totalSeconds > seconds) {
+      totalSeconds -= seconds;
+      calculateTime();
+    } else {
+      ErrorService.setMessage("NEGATIVE_TIME");
+    }
+  };
+  addSeconds = function(seconds) {
+    totalSeconds += seconds;
+    calculateTime();
+  };
+  timer = function() {
+    ++totalSeconds;
+    calculateTime();
+    timerIsRunning = true;
+  };
+  toMinutes = function(seconds) {
+    var minutes;
+    minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+      return '0' + minutes;
+    } else {
+      return minutes;
+    }
+  };
+  calculateTime = function() {
+    var seconds;
+    seconds = totalSeconds % 60;
+    totalMinutes = toMinutes(totalSeconds);
+    playMinutes = toMinutes(totalSeconds + 60);
+    time = totalMinutes + ":" + (seconds < 10 ? '0' + seconds : seconds);
   };
   TimerService.$inject = ['$interval', 'ErrorService', 'SettingsService'];
-  return angular.module("timer.service", []).service("TimerService", TimerService);
+  return angular.module("timer.service", []).factory("TimerService", function() {
+    return new TimerService();
+  });
 })();
 
 (function() {
